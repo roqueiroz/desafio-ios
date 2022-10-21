42ccf3bf1bb4d2e0c5b346df4629921ed9ccf1f5
Rodrigo Gonçalves de Queiroz <rodrigo.goncalves@DBC-001068.local>
2022-10-21 17:40:22 -0300

    create project

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Rodrigo Gonçalves de Queiroz on 21/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
